8497aa4229fef82c2103d7bf3c1bb5c145d1abc3
alfre <alfredo.medina@uc.cl>
2024-04-27 12:13:16 -0400

    add tanstack query and react-router

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { NextUIProvider } from '@nextui-org/react'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <NextUIProvider>
+          <App />
+        </NextUIProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
