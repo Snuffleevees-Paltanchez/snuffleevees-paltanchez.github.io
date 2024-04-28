1c553c2eb69072a9a172418ae5e6b35db89cbe05
AlfreMK <alfredmkzz@gmail.com>
2024-04-28 15:55:12 -0400

    address basti comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { NextUIProvider } from '@nextui-org/react'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App.tsx'
 
 const queryClient = new QueryClient()
 
