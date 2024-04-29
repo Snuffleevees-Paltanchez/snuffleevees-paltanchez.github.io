bc93032b97319f40824032954f92da78c9ad0996
AlfreMK <alfredmkzz@gmail.com>
2024-04-28 23:57:11 -0400

    fix: routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Navbar from '@/components/navbar/Navbar'
 import LandingPage from '@/pages/LandingPage'
 import NotFound from '@/pages/NotFound'
 
+const BASE_URL = import.meta.env.VITE_BASE_URL || '/mikbooks'
+
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path={`${BASE_URL}/`} element={<LandingPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
