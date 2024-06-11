4303ec486336e56a7a5b9713b25f9ddc4f2a9bdb
alfre <alfredo.medina@uc.cl>
2024-06-10 22:59:11 -0400

    revert

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,3 @@
-import BookCard from '@/components/BookCard'
-
 export default function LandingPage() {
-  return (
-    <div>
-      <BookCard isbn="9788417254353" price={18000} />
-    </div>
-  )
+  return <div>Hello World!</div>
 }
