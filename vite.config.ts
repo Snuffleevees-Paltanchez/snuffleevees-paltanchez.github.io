1b0999f27d01648a94070aa1f535da5b561d2de5
Alfredo Medina Klapp <alfredmkzz@gmail.com>
2024-06-21 19:13:00 -0400

    chore: coverage (#22)
    * feat: add base coverage
    
    * add: istanbul
    
    * add: coverage in gitignore
    
    * linter
    
    ---------
    
    Co-authored-by: bastian <b.marinkovic@uc.cl>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import istanbul from 'vite-plugin-istanbul'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
